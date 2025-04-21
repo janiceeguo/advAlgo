ba0495f07755689b982f55a119e5bf96e0ac2618
Edward Wei <nyw6dh@virginia.edu>
2025-04-20 23:37:08 -0400

    add cpp comments

--- projects/01-primalityTests/programingChallenge/solutions/pcSol_cpp.cpp
+++ projects/01-primalityTests/programingChallenge/solutions/pcSol_cpp.cpp
@@ -6,13 +6,17 @@
 
 std::mt19937 engine(std::random_device{}());
 
+// Use binary exponentiation to compute a^b mod m
 u128 binPow(u128 a, u128 b, u128 m) {
 	u128 r = 1;
-	a %= m;
+	a %= m;	 // If a >= m, we can reduce it
+
 	while (b > 0) {
+		// If b is odd, multiply a with result
 		if (b & 1) {
 			r = (r * a) % m;
 		}
+
 		b >>= 1;
 		a = (a * a) % m;
 	}
@@ -21,8 +25,10 @@ u128 binPow(u128 a, u128 b, u128 m) {
 
 bool checkComposite(u128 n, u128 a, u128 s, u128 d) {
 	u128 r = binPow(a, d, n);
+	// probably prime
 	if (r == 1 || r == n - 1) return false;
 
+	// Check the factors
 	for (u128 i = 1; i < s; ++i) {
 		r = binPow(r, 2, n);
 		if (r == n - 1) return false;
@@ -34,6 +40,7 @@ bool checkComposite(u128 n, u128 a, u128 s, u128 d) {
 bool isPrime(u128 n) {
 	if (n < 4) return n == 2 || n == 3;
 
+	// Compute 2^s * d = n - 1
 	u128 a, s = 0;
 	u128 d = n - 1;
 	while ((d & 1) == 0) {
@@ -43,6 +50,7 @@ bool isPrime(u128 n) {
 
 	std::uniform_int_distribution<u128> dist(2, n - 2);
 
+	// Witness loop
 	for (int i = 0; i < K; ++i) {
 		a = dist(engine);
 		if (checkComposite(n, a, s, d)) {
@@ -53,6 +61,7 @@ bool isPrime(u128 n) {
 	return true;
 }
 
+// Hash function for fartcoin
 u128 fartcoinHash(std::string &s) {
 	u128 h = 0;
 	for (char c : s) {
@@ -78,6 +87,7 @@ int main(int argc, char *argv[]) {
 		h = fartcoinHash(temp);
 
 		if (isPrime(h)) {
+			// Handle adding to chain
 			chainLength++;
 			chain = temp;
 			std::cout << name << " accepted " << chainLength << std::endl;
